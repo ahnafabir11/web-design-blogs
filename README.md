1ae2fb5279de8574819d6d13aebb1c714e9d04d5
ahnafabir <ahnafabir313@gmail.com>
2021-05-08 20:15:05 +0600

    overlay done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Web Design Blogs
+
+This is a blog site for web designers. There are 4 blogs all about web design. This is a frontend intern test given by Bwow Health & Fitness Private Limited. 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,11 +2,12 @@ import './Home.css';
 import React, { useContext, useEffect, useState } from 'react';
 import Blog from '../Blog/Blog';
 import Modal from '../Modal/Modal';
-import { BlogContext } from '../../App';
+import { BlogContext, ModalContext } from '../../App';
 
 const Home = () => {
   const [blogs, setBlogs] = useState([]);
   const [selectedBlog] = useContext(BlogContext);
+  const [isModalOpen] = useContext(ModalContext);
 
   useEffect(() => {
     fetch(`https://my-json-server.typicode.com/Codeinwp/front-end-internship-api/posts`)
@@ -19,7 +20,10 @@ const Home = () => {
       {
         blogs.map(blog => <Blog key={blog.id} blog={blog} />)
       }
+      <div className="overlay" style={{ opacity: isModalOpen ? 1 : 0 }}></div>
       <Modal blog={selectedBlog} />
+
+      
     </div>
   );
 };
